4b3b06cad8751b9a1b20cb8d798a673837e48030
EmilioZorzi01 <134383470+EmilioZorzi01@users.noreply.github.com>
2026-02-02 13:53:43 +0100

    Delete Code/Data_Extraction.R

--- Code/Data_Extraction.R
+++
@@ -1,7 +0,0 @@
-# data extraction
-
-data = read_xlsx("C:\\Users\\mauri\\Desktop\\Datan.xlsx")
-data = dummy_cols(data, select_columns = "Parsimony", remove_first_dummy = FALSE)
-data = data %>% 
-  mutate(across(everything(), ~as.numeric(as.character(.))))
-attach(data)
